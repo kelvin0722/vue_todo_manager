f5604262379308b7558574ddeef5b8fb9a0b8a4a
kelvin0722 <kelvinm442@gmail.com>
2019-03-24 19:03:14 +0300

    create fetchTodo action and mutations

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,7 +2,7 @@
    <div>
        <h3>Todos</h3>
        <div class="todos">
-           <div v-for="(todo, index) in allTodos" :key="index">
+           <div v-for="todo in allTodos" :key="todo.id" class="todo">
                {{todo.title}}
            </div>
        </div>
@@ -10,15 +10,36 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
     name: 'Todos',
-    computed: mapGetters(['allTodos'])
+    methods: {
+        ...mapActions(['fetchTodos'])
+    },
+    computed: mapGetters(['allTodos']),
+    created(){
+        this.fetchTodos()
+    }
 
 }
 </script>
 
-<style>
+<style scoped>
+  .todos {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 1rem;
+  }
 
+  .todo {
+      border: 1px solid #ccc;
+      background: #41b883;
+      padding: 1rem;
+      border-radius: 5px;
+      text-align: center;
+      position: relative;
+      cursor: pointer;
+  }
 </style>
+
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,26 +1,28 @@
-// import axios from "axios"
+import axios from "axios"
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo One'
-        },
-        {
-            id: 1,
-            title: 'Todo two'
-        }
-    ]
+    todos: []
 }
 
 const getters  = {
     allTodos: state => state.todos
 }
 
-const mutations = {}
+const actions = {
+    async fetchTodos({ commit }) {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+
+     commit('setTodos', response.data)
+    }
+}
+
+const mutations = {
+    setTodos: (state, todos) => state.todos = todos
+}
 
 export default {
     state,
     getters,
+    actions,
     mutations,
 }
